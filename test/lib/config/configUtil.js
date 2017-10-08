9eedcf25d3e96eb1f4a1183d5c86b6dcb141f303
Jeff Brateman <jeff.brateman@getbraintree.com>
2017-10-08 14:15:41 -0500

    Lint unit tests

--- test/lib/config/configUtil.js
+++ test/lib/config/configUtil.js
@@ -1,47 +1,47 @@
 'use strict';
 
-var _ = require('lodash');
-var chai = require('chai');
-var assert = chai.assert;
-var path = require('path');
-var configUtil = require(path.resolve('lib/config/configUtil'))();
+const chai = require('chai');
 
-describe('getFirstMatchingPattern', function () { 
-  it('should return a matching pattern when the first matches', function () {
-    var patterns = ["https:\\/\\/example\\.com"];
+const configUtil = require('../../../lib/config/configUtil')();
 
-    assert.equal(patterns[0], configUtil.getFirstMatchingPattern(patterns, "https://example.com"));
+const assert = chai.assert;
+
+describe('getFirstMatchingPattern', () => {
+  it('should return a matching pattern when the first matches', () => {
+    const patterns = ['https:\\/\\/example\\.com'];
+
+    assert.equal(patterns[0], configUtil.getFirstMatchingPattern(patterns, 'https://example.com'));
   });
 
-  it('should return the first matching pattern when more than one matches', function () {
-    var patterns = ["https:\\/\\/example\\.com", "com", "example"];
+  it('should return the first matching pattern when more than one matches', () => {
+    const patterns = ['https:\\/\\/example\\.com', 'com', 'example'];
 
-    assert.equal(patterns[0], configUtil.getFirstMatchingPattern(patterns, "https://example.com"));
-    assert.equal(patterns[1], configUtil.getFirstMatchingPattern(patterns, "https://github.com"));
-    assert.equal(patterns[2], configUtil.getFirstMatchingPattern(patterns, "https://example.org"));
+    assert.equal(patterns[0], configUtil.getFirstMatchingPattern(patterns, 'https://example.com'));
+    assert.equal(patterns[1], configUtil.getFirstMatchingPattern(patterns, 'https://github.com'));
+    assert.equal(patterns[2], configUtil.getFirstMatchingPattern(patterns, 'https://example.org'));
   });
 
-  it('should return undefined when it does not match', function () {
-    var patterns = ["https:\\/\\/example\\.com"];
+  it('should return undefined when it does not match', () => {
+    const patterns = ['https:\\/\\/example\\.com'];
 
-    assert.isUndefined(configUtil.getFirstMatchingPattern(patterns, "https://github.com"));
+    assert.isUndefined(configUtil.getFirstMatchingPattern(patterns, 'https://github.com'));
   });
 
-  it('should return undefined when the patterns is empty', function () {
-    assert.isUndefined(configUtil.getFirstMatchingPattern(undefined, "https://example.com"));
-    assert.isUndefined(configUtil.getFirstMatchingPattern([], "https://example.com"));
+  it('should return undefined when the patterns is empty', () => {
+    assert.isUndefined(configUtil.getFirstMatchingPattern(undefined, 'https://example.com'));
+    assert.isUndefined(configUtil.getFirstMatchingPattern([], 'https://example.com'));
   });
 
-  it('should return the first matching ROOT_URL replacement', function () {
-    var patterns = [ "^#{ROOT_URL}\\/lala\\/$" ];
+  it('should return the first matching ROOT_URL replacement', () => {
+    const patterns = ['^#{ROOT_URL}\\/lala\\/$'];
 
-    assert.equal(patterns[0], 
-      configUtil.getFirstMatchingPattern(patterns, "https://localhost:9999/lala/", "https://localhost:9999"));
+    assert.equal(patterns[0],
+      configUtil.getFirstMatchingPattern(patterns, 'https://localhost:9999/lala/', 'https://localhost:9999'));
   });
 
-  it('should return undefined if the ROOT_URL replacement still does not match', function () {
-    var patterns = [ "^#{ROOT_URL}\\/lala\\/$" ];
+  it('should return undefined if the ROOT_URL replacement still does not match', () => {
+    const patterns = ['^#{ROOT_URL}\\/lala\\/$'];
 
-    assert.isUndefined(configUtil.getFirstMatchingPattern(patterns, "https://example.com", "https://localhost:9999"));
+    assert.isUndefined(configUtil.getFirstMatchingPattern(patterns, 'https://example.com', 'https://localhost:9999'));
   });
 });
